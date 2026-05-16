7e1ca1fe77f3004ba173c1328697ab83be8387e0
EddyAdr <razanakotoandrianinaeddy199@gmail.com>
2026-05-16 20:37:40 +0300

    Premier commit

---
+++ Gestion_de_salle.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GSE
+{
+	public partial class Gestion_de_salle : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
